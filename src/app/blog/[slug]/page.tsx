f555dbb9c28d2212ee929744c0ea3f03ff5898d1
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-05-26 18:07:45 -0600

    Start on styling individual blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,14 @@
 import { GET_SINGLE_BLOG_POST } from "../../api/graphql/queries";
 import { getClient } from "../../ApolloClient";
 import parse from "html-react-parser";
+import Image from "next/image";
+import styles from "../blog.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
+
+const calendarIcon = (
+  <FontAwesomeIcon icon={faCalendarDays} className="w-4 mx-2" />
+);
 
 interface BlogPageParams {
   params: Promise<{
@@ -20,6 +28,7 @@ interface BlogPost {
 }
 
 const BlogPage = async ({ params }: BlogPageParams) => {
+  const hostname = process.env.NEXT_PUBLIC_IMAGE_PATH;
   const { slug } = await params;
   let blog = {} as BlogPost;
   try {
@@ -35,15 +44,39 @@ const BlogPage = async ({ params }: BlogPageParams) => {
   }
 
   return (
-    <>
-      <h1>Blog Page</h1>
-      {blog?.title && (
-        <div>
-          <h1>{blog?.title}</h1>
+    <article className={`blog flex flex-col w-full`}>
+      <div className="img-wrapper h-[230px] lg:h-[405px] relative mb-8 lg:mb-14">
+        <Image
+          src={`${hostname}${blog.image}`}
+          alt={`Photo for ${blog.title}`}
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+      </div>
+      <section className="grid grid-cols-12 justify-center">
+        {blog?.title && (
+          <div className="col-span-10 col-start-2 lg:col-span-8 lg:col-start-3">
+            <h1 className="text-h1-sm lg:text-h1 font-kanit font-bold text-center">
+              {blog?.title}
+            </h1>
+            <span className="flex flex-nowrap justify-center items-center text-black text-intro-min-sm md:text-intro mb-8 mt-3 lg:mt-2">
+              {calendarIcon}
+              <p className="text-black text-body-min-sm md:text-intro-sm text-center">
+                {new Date(blog.createdAt).toLocaleDateString()}
+              </p>
+            </span>
+          </div>
+        )}
+        <div
+          className={`${styles.blog} blog-body col-span-10 col-start-2 md:col-span-8 md:col-start-3 lg:col-span-6 lg:col-start-4 text-black text-body-sm lg:text-body`}
+        >
           {parse(blog.content)}
         </div>
-      )}
-    </>
+      </section>
+    </article>
   );
 };
 
