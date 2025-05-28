7a354b3d594b8285d54d7f1a76c0970230648d7f
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-05-28 10:31:57 -0600

    Start on styling individual blog page (#5)
    * Start on styling individual blog page
    
    * Update styles on blog pages and add date library
    
    * Update button position on mobile

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,16 @@
 import { GET_SINGLE_BLOG_POST } from "../../api/graphql/queries";
 import { getClient } from "../../ApolloClient";
 import parse from "html-react-parser";
+import styles from "../blog.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
+import { Suspense } from "react";
+import BlogPageImage from "@/components/BlogPageImage";
+import dayjs from "dayjs";
+
+const calendarIcon = (
+  <FontAwesomeIcon icon={faCalendarDays} className="w-4 mx-2" />
+);
 
 interface BlogPageParams {
   params: Promise<{
@@ -8,7 +18,7 @@ interface BlogPageParams {
   }>;
 }
 
-interface BlogPost {
+export interface BlogPost {
   id: number;
   title: string;
   subtitle: string;
@@ -33,17 +43,46 @@ const BlogPage = async ({ params }: BlogPageParams) => {
   } catch (error) {
     console.error("Error fetching blog posts:", error);
   }
+  const date = dayjs(blog.createdAt, "MMM D, YYYY");
 
   return (
-    <>
-      <h1>Blog Page</h1>
-      {blog?.title && (
-        <div>
-          <h1>{blog?.title}</h1>
-          {parse(blog.content)}
+    <Suspense fallback={<div>Loading...</div>}>
+      <article className={`blog flex flex-col w-full`}>
+        <div
+          className="flex items-center justify-center img-wrapper h-[230px] lg:h-[405px] relative mb-8
+          l<g:mb-14"
+        >
+          <BlogPageImage blog={blog} />
         </div>
-      )}
-    </>
+        <section className="grid grid-cols-12 justify-center">
+          {blog?.title && (
+            <div className="col-span-10 col-start-2 lg:col-span-8 lg:col-start-3">
+              <h1 className="text-h1-sm lg:text-h1 font-kanit font-bold text-center">
+                {blog?.title}
+              </h1>
+            </div>
+          )}
+          <div className="col-span-10 col-start-2 md:col-span-8 md:col-start-3 lg:col-span-6 lg:col-start-4 text-black text-body-sm lg:text-body">
+            <div className="intro mt-6 mb-10 lg:mt-8 lg:mb-12 text-primary">
+              {blog.subtitle && (
+                <p className="text-intro-sm lg:text-intro text-center">
+                  {blog.subtitle}
+                </p>
+              )}
+              <span className="flex flex-nowrap justify-center items-center text-primary text-intro-min-sm md:text-intro mb-8 mt-3 lg:mt-2">
+                {calendarIcon}
+                <p className="text-primary text-body-min-sm md:text-intro-sm text-center">
+                  {date.format("MMM D, YYYY")}
+                </p>
+              </span>
+            </div>
+            <div className={`${styles.blog} blog-body`}>
+              {parse(blog.content)}
+            </div>
+          </div>
+        </section>
+      </article>
+    </Suspense>
   );
 };
 
--- src/app/components/BlogPostFeatured.tsx
+++ src/app/components/BlogPostFeatured.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import parse from "html-react-parser";
 import { Post } from "../blog/page";
 import Button from "./Button";
+import dayjs from "dayjs";
 
 const BlogPostFeatured = ({ post }: { post: Post }) => {
   const hostname = process.env.NEXT_PUBLIC_IMAGE_PATH;
+  const date = dayjs(post.createdAt, "MMM D, YYYY");
   return (
     <div className="flex flex-wrap items-center justify-center relative w-full h-[405px] mb-16">
       <div className="overlay absolute top-o bottom-0 left-0 right-0 w-full h-full z-10 bg-black opacity-60"></div>
@@ -20,23 +22,25 @@ const BlogPostFeatured = ({ post }: { post: Post }) => {
           backgroundColor: "black",
         }}
       />
-      <div className="grid grid-cols-12 absolute mx-auto text-center left-0 right-0 z-20">
-        <h1 className="col-span-10 col-start-2 xl:col-span-8 xl:col-start-3 w-full text-h1-sm md:text-h2 lg:text-h1 text-white font-kanit text-shadow-xl mb-3">
+      <div className="grid grid-cols-12 h-full absolute mx-auto text-center left-0 right-0 z-20">
+        <h1 className="self-end col-span-10 col-start-2 xl:col-span-8 xl:col-start-3 w-full text-h1-sm md:text-h2 lg:text-h1 text-white font-kanit text-shadow-xl mb-3">
           {post.title}
         </h1>
-        <div className="col-span-10 col-start-2 xl:col-span-6 xl:col-start-4">
+        <div className="h-full justify-between lg:justify-start flex flex-col col-span-10 col-start-2 xl:col-span-6 xl:col-start-4">
           <p className="text-white text-body-min-sm md:text-body-min text-shadow-xl my-4">
-            {new Date(post.createdAt).toLocaleDateString()}
+            {date.format("MMM D, YYYY")}
           </p>
           <span className="block text-white text-intro-sm lg:text-intro text-shadow-xl mb-6 w-full">
             {parse(post.excerpt)}
           </span>
-          <Button
-            size="large"
-            type="primary"
-            text="Read More"
-            link={`/blog/${post.slug}`}
-          />
+          <div className="wrapper mt-auto -mb-[18px] lg:mb-0 lg:mt-0">
+            <Button
+              size="large"
+              type="primary"
+              text="Read More"
+              link={`/blog/${post.slug}`}
+            />
+          </div>
         </div>
       </div>
     </div>
