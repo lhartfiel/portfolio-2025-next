94e50d8a3f3a4c04ec65c10e30ddd09778177b84
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-06-25 14:23:14 -0600

    Add fallbacks in next config

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,24 @@ import type { NextConfig } from "next";
 
 const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_PATH;
 const DOMAIN_PATH = process.env.NEXT_SERVER_PATH;
-const url = new URL(IMAGE_URL as string);
+let url: URL | undefined;
+if (IMAGE_URL) {
+  url = new URL(IMAGE_URL);
+}
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [`${DOMAIN_PATH}`],
-    remotePatterns: [
-      {
-        protocol: url.protocol.replace(":", "") as "http" | "https",
-        hostname: url.hostname,
-        port: url.port || undefined,
-        pathname: "/media/uploads/**",
-      },
-    ],
+    domains: DOMAIN_PATH ? [DOMAIN_PATH] : [],
+    remotePatterns: url
+      ? [
+          {
+            protocol: url.protocol.replace(":", "") as "http" | "https",
+            hostname: url.hostname,
+            port: url.port || undefined,
+            pathname: "/media/uploads/**",
+          },
+        ]
+      : [],
   },
 };
 
